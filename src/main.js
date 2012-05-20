78415f5e919736d281b386522c2af9a771cde2f3
Ryan Rix <ry@n.rix.si>
2012-05-19 18:12:31 -0700

    Test pane animation with custom component pushing

--- src/main.js
+++ src/main.js
@@ -21,9 +21,15 @@ enyo.kind({
         kind: 'onyx.Toolbar',
         layoutKind: 'FittableColumnsLayout',
         components: [
-          {kind: 'onyx.Button', allowHtml: true, content: '&larr; Back'},
+          {kind:      'onyx.Button',
+           allowHtml: true,
+           content:   '&larr; Back',
+           onclick:   'previousSlide'},
           {fit: true},
-          {kind: 'onyx.Button', allowHtml: true, content: 'Next &rarr;'}
+          {kind:      'onyx.Button',
+           allowHtml: true,
+           content:   'Next &rarr;',
+           onclick:   'nextSlide'}
         ]
       }
     ]}
@@ -32,9 +38,21 @@ enyo.kind({
   create: function() {
     this.inherited(arguments);
 
-    var component = { kind: "Slides.Slide", name: "slide1", content: "Hello world!" };
+    var component = { kind: "Slides.Slide", name: "slide1", content: "Hello world 1!" };
     this.$.slidesPanes.viewTypes.push( component );
-    this.$.slidesPanes.push( "slide1" );
+    component = { kind: "Slides.Slide", name: "slide2", content: "Hello world 2!" };
+    this.$.slidesPanes.viewTypes.push( component );
+    component = { kind: "Slides.Slide", name: "slide3", content: "Hello world 3!" };
+    this.$.slidesPanes.viewTypes.push( component );
+  },
+
+  nextSlide: function() {
+    component = this.$.slidesPanes.viewTypes[this.$.slidesPanes.getViewCount()];
+    this.$.slidesPanes.push(component.name);
+  },
+
+  previousSlide: function() {
+    this.$.slidesPanes.getView().pop();
   }
 });
 
