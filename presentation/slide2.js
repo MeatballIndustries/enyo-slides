1df94e81c556fea00241ec084dc18154fd49b210
Ryan Rix <ry@n.rix.si>
2012-06-16 07:27:02 -0700

    Make slide2 use panels to be responsive on small UIs

--- presentation/slide2.js
+++ presentation/slide2.js
@@ -1,16 +1,19 @@
 enyo.kind({
   kind: "Slides.Slide",
   name: "slide2",
+  layoutKind: "enyo.FittableRowsLayout",
+  classes: "enyo-fit",
 
   components: [
     {kind: "onyx.Toolbar", components: [
       {tag: "h1", content: "Who are we?"}
     ]},
-    {tag: "div", name: "left_div", classes: "left-div", components: [
-      {content: "Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions. Completely synergize resource sucking relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service. Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing procedures for reliable supply chains. Dramatically engage top-line web services vis-a-vis cutting-edge deliverables. Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing. Holistically pontificate installed base portals after maintainable products."}
-    ]},
-    {name: "right_div", classes: "right-div", components: [
-      {components: [
+    // XXX: 
+    {kind: "Panels", fit:true, classes: "responsive-panels", arrangerKind: "CollapsingArranger", components: [
+      {classes: "left-div", components: [
+        {content: "Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions. Completely synergize resource sucking relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service. Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing procedures for reliable supply chains. Dramatically engage top-line web services vis-a-vis cutting-edge deliverables. Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing. Holistically pontificate installed base portals after maintainable products."}
+      ]},
+      {fit: true, components: [
         {kind: "Image", src: "http://rix.si/assets/uploads/ryan_headshot.png", classes: "left-photo"},
         {components: [
           {tag: "span", classes: "strong", content: "Ryan Rix"},
@@ -19,23 +22,19 @@ enyo.kind({
           {content: "By night, Ryan builds rockets, VPN darknets, communities and 3D printed dongs as one of the board members of HeatSync Labs, a hackerspace and community workshop in Mesa specializing in teaching and learning all forms of creation as well as pushing the limits of technologies of all kinds."},
           {tag: "br"},
         ]},
-      ]},
-      {components: [
-        {tag: "span", content: "As seen on: "},
-        {tag: "a", attributes: { href: "http://rix.si", }, content: "The blagosphere" },
-        {tag: "span", content: " - "},
-        {tag: "a", attributes: { href: "http://github.com/rrix", }, content: "Github" },
-        {tag: "span", content: " - "},
-        {tag: "a", attributes: { href: "http://rebelhold.com", }, content: "RebelHold" },
-        {tag: "span", content: " - "},
-        {tag: "a", attributes: { href: "http://twitter.com/rrrrrrrix", }, content: "Tweeters" },
-        {tag: "span", content: " - "},
-        {tag: "a", attributes: { href: "http://heatsynclabs.org", }, content: "HeatSync Labs" },
+        {components: [
+          {tag: "span", content: "As seen on: "},
+          {tag: "a", attributes: { href: "http://rix.si", }, content: "The blagosphere" },
+          {tag: "span", content: " - "},
+          {tag: "a", attributes: { href: "http://github.com/rrix", }, content: "Github" },
+          {tag: "span", content: " - "},
+          {tag: "a", attributes: { href: "http://rebelhold.com", }, content: "RebelHold" },
+          {tag: "span", content: " - "},
+          {tag: "a", attributes: { href: "http://twitter.com/rrrrrrrix", }, content: "Tweeters" },
+          {tag: "span", content: " - "},
+          {tag: "a", attributes: { href: "http://heatsynclabs.org", }, content: "HeatSync Labs" },
+        ]}
       ]}
     ]}
-  ],
-
-  create: function() {
-    this.inherited(arguments);
-  }
+  ]
 });
