cbcf58824bedce97599f5e0c2150b4f0fa108807
Blaine Bublitz <phate666x@gmail.com>
2012-06-21 14:29:35 -0700

    aboutUs tweaks

--- presentation/slides/aboutUs.js
+++ presentation/slides/aboutUs.js
@@ -11,35 +11,146 @@ enyo.kind({
       components:[
         {
           fit: true,
-          name: "left_div"
+          name: "left_div",
+          components: [
+            {
+              components: [
+                {
+                  components: [
+                    {
+                      kind: 'enyo.Image',
+                      classes: 'right-photo',
+                      src: '$presentation/assets/images/blaine.jpg'
+                    },
+                    {
+                      tag: 'ul',
+                      components: [
+                        {
+                          tag: 'li',
+                          components: [
+                            {
+                              content: 'Blaine Bublitz'
+                            },
+                            {
+                              tag: 'ul',
+                              components: [
+                                {
+                                  tag: 'li',
+                                  content: 'Kind of a big deal'
+                                },
+                                {
+                                  tag: 'li',
+                                  content: 'Co-founded Iced Development'
+                                },
+                                {
+                                  tag: 'li',
+                                  content: 'Works on JavaScript/HTML5 and Node.js'
+                                },
+                                {
+                                  tag: 'Likes complicated front-end JavaScript projects'
+                                },
+                                {
+                                  tag: 'li',
+                                  content: 'Does a lot of toolchain development'
+                                },
+                                {
+                                  tag: 'li',
+                                  content: 'Interested in Responsive/mobile-first design'
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              style: 'text-align: center;',
+              components: [
+                {tag: "a", attributes: { href: "http://iceddev.com" }, content: "Iced Dev" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://github.com/phated" }, content: "Github" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://twitter.com/BlaineBublitz" }, content: "Blaine on Twitter" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://twitter.com/iceddev" }, content: "Iced on Twitter" }
+              ]
+            }
+          ]
         },
         {
           name: "right_div",
           style: 'width: 50%;',
           components: [
-            {components: [
-              {kind: "Image", src: "$presentation/assets/ryan_headshot_med.jpg", classes: "left-photo"},
-              {components: [
-                {tag: "span", classes: "strong", content: "Ryan Rix"},
-                {tag: "span", content: " is an open source developer born in raised in the Valley. He has been developing mobile and desktop applications since 2005, and is involved in a number open source projects."},
-                {content: "By day, Ryan is a Ruby on Rails developer with Rebelhold, LLC out of HeatSync Labs in Mesa, working on a number of client projects as well as open-source endevours."},
-                {content: "By night, Ryan builds rockets, VPN darknets, communities and 3D printed dongs as one of the board members of HeatSync Labs, a hackerspace and community workshop in Mesa specializing in teaching and learning all forms of creation as well as pushing the limits of technologies of all kinds."},
-                {tag: "br"}
-              ]}
-            ]},
-            {components: [
-              {tag: "span", content: "As seen on: "},
-              {tag: "a", attributes: { href: "http://rix.si" }, content: "The blagosphere" },
-              {tag: "span", content: " - "},
-              {tag: "a", attributes: { href: "http://github.com/rrix" }, content: "Github" },
-              {tag: "span", content: " - "},
-              {tag: "a", attributes: { href: "http://rebelhold.com" }, content: "RebelHold" },
-              {tag: "span", content: " - "},
-              {tag: "a", attributes: { href: "http://twitter.com/rrrrrrrix" }, content: "Tweeters" },
-              {tag: "span", content: " - "},
-              {tag: "a", attributes: { href: "http://heatsynclabs.org" }, content: "HeatSync Labs" }
-            ]}
-]
+            {
+              components: [
+                {
+                  kind: "enyo.Image",
+                  classes: "right-photo",
+                  src: "$presentation/assets/images/ryan_headshot_med.jpg"
+                },
+                {
+                  tag: 'ul',
+                  components: [
+                    {
+                      tag: 'li',
+                      components: [
+                        {
+                          content: 'Ryan Rix'
+                        },
+                        {
+                          tag: 'ul',
+                          components: [
+                            {
+                              tag: 'li',
+                              content: 'Also, kind of a big deal'
+                            },
+                            {
+                              tag: 'li',
+                              content: 'HeatSync Labs board member'
+                            },
+                            {
+                              tag: 'li',
+                              content: 'Open Source Contributor'
+                            },
+                            {
+                              tag: 'li',
+                              content: 'Qt and Enyo Developer'
+                            },
+                            {
+                              tag: 'li',
+                              content: 'Day job: Ruby on Rails Dev with Rebelhold'
+                            },
+                            {
+                              tag: 'li',
+                              content: 'At night: 3D Printer, Rocketry, Hacker/Maker'
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              style: 'text-align: center;',
+              components: [
+                {tag: "a", attributes: { href: "http://rix.si" }, content: "The blagosphere" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://github.com/rrix" }, content: "Github" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://rebelhold.com" }, content: "RebelHold" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://twitter.com/rrrrrrrix" }, content: "Tweeters" },
+                {tag: "span", content: " - "},
+                {tag: "a", attributes: { href: "http://heatsynclabs.org" }, content: "HeatSync Labs" }
+              ]
+            }
+          ]
         }
       ]
     }
