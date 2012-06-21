6311fc3e3740175cd5ca6755f50f8cc81c3de232
Ryan Rix <ry@n.rix.si>
2012-06-20 19:02:34 -0700

    Modify onyxControls sample to show off sub-kinding onyx components

--- presentation/assets/code/onyxControls.js
+++ presentation/assets/code/onyxControls.js
@@ -1,52 +1,46 @@
 enyo.kind({
-  kind: "enyo.Control",
-  name: "OnyxControlsDemo",
-
+  kind: "onyx.Groupbox",
+  name: "sampleGroupBox",
+  fit: true,
   components: [
     {
-      kind: "onyx.Groupbox",
-      fit: true,
+      kind: "onyx.GroupboxHeader", 
+      content: "GROUP BOXEN"
+    },
+    {
+      kind: "Group",
+      name: "toggleGroup",
+      defaultKind: "onyx.ToggleButton", components: [
+        {value: true},
+        {},
+        {}
+      ]
+    },
+    {
+      kind: "Group",
+      name: "checkGroup",
+      defaultKind: "onyx.Checkbox", components: [
+        {value: true},
+        {},
+        {}
+      ]
+    },
+    {
+      kind: "Group",
+      name: "buttonGroup",
+      defaultKind: "onyx.Button", components: [
+        {content: "Hello 1"},
+        {content: "Hello 2"},
+        {content: "Hello 3"}
+      ]
+    },
+    {
+      kind: "Group",
+      defaultKind: "onyx.IconButton",
       components: [
-        {
-          kind: "onyx.GroupboxHeader", 
-          content: "GROUP BOXEN"
-        },
-        {
-          kind: "Group",
-          name: "toggleGroup",
-          defaultKind: "onyx.ToggleButton", components: [
-            {value: true},
-            {},
-            {}
-          ]
-        },
-        {
-          kind: "Group",
-          name: "checkGroup",
-          defaultKind: "onyx.Checkbox", components: [
-            {value: true},
-            {},
-            {}
-          ]
-        },
-        {
-          kind: "Group",
-          name: "buttonGroup",
-          defaultKind: "onyx.Button", components: [
-            {content: "Hello 1"},
-            {content: "Hello 2"},
-            {content: "Hello 3"}
-          ]
-        },
-        {
-          kind: "Group",
-          defaultKind: "onyx.IconButton",
-          components: [
-            {src: "$lib/onyx/images/progress-button-cancel.png", active: true,},
-            {src: "$lib/onyx/images/progress-button-cancel.png"},
-            {src: "$lib/onyx/images/progress-button-cancel.png"}
-          ]
-        }
+        {src: "$lib/onyx/images/progress-button-cancel.png", active: true,},
+        {src: "$lib/onyx/images/progress-button-cancel.png"},
+        {src: "$lib/onyx/images/progress-button-cancel.png"}
       ]
     }
   ]
